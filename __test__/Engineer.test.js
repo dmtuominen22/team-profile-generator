f63a260521c2db74ae2a1283ed9097f48899f6b8
Denise <dmtuominen@gmail.com>
2021-08-21 14:41:24 -0600

    adding engineer

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,6 +1,8 @@
-const { test } = require('@jest/globals');
-const Engineer = require('../lib/Engineer');
+const Engineer = require('../lib/Engineer')
 
-test('can get gitHub account via constructor', () =>{
+ test('can get GitHub username via constructor', () => {
+    const github = "GitHubUser";
+    const employee = New Engineer('Lisa', 'E2345', 'testing@test.com', github, 300,title);
 
-})
+    expect(employee.github).tobe(github);
+ });
