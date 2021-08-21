e17f198994f38dc9cb0f0a432bf55e2cfca3c927
Denise <dmtuominen@gmail.com>
2021-08-21 15:21:35 -0600

    added all of the meployees

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,8 +1,16 @@
-const Engineer = require('../lib/Engineer')
+const Engineer = require ('../lib/Engineer');
 
  test('can get GitHub username via constructor', () => {
-    const github = "GitHubUser";
-    const employee = New Engineer('Lisa', 'E2345', 'testing@test.com', github, 300,title);
+    const gitHubValue = "GitHubUser";
+    const employee = new Engineer('Lisa', 'E2345', 'testing@test.com', gitHubValue, 300, 'Engineer');
 
-    expect(employee.github).tobe(github);
+    expect(employee.github).toBe(gitHubValue);
  });
+
+ test('can get title via constructor', () => {
+    const title = 'Engineer';
+    const engineer = new Engineer('Lisa', 'E2345', 'testing@test.com', 'dmtuominen22', 500, title);
+
+    expect(engineer.title).toBe(title);
+
+});
--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,1 +1,16 @@
 const Intern = require ('../lib/Intern');
+
+test('can get school via constructor', () => {
+    const school = "school";
+    const employee = new Intern('Lisa', 'E2345', 'testing@test.com', school, 'dmtuominen22', 300, 'Engineer');
+
+    expect(employee.school).toBe(school);
+ });
+
+test('can get title via constructor', () => {
+    const title = 'Intern';
+    const intern = new Intern('Lisa', 'E2345', 'testing@test.com', ' Berkley','dmtuominen22', 500, title);
+
+    expect(intern.title).toBe(title);
+
+ });
