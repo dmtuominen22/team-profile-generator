902609258e1c7f404463f2bcc28d07e03fd7928a
Denise <dmtuominen@gmail.com>
2021-08-25 19:52:25 -0600

    added questions prompt

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+
 const employees = [];
+
 
 // team manager’s name, employee ID, email address, and office number
 const promptUser = () => {
@@ -12,26 +14,45 @@ const promptUser = () => {
         .prompt([
             {
                 type: "input",
-                name: "managerName",
-                messsage: "What is the team Manager's Name?",
+                name: "name",
+                messsage: "What is the team Members Name?",
                 validate: (nameInput) => {
                     if (nameInput) {
                         return true;
                     } else {
-                        console.log("Please enter the team Manager's Name!");
+                        console.log("Please enter the team Members's Name!");
+                    }
+                },
+            },
+
+            {
+                type: "list",
+                name: "role",
+                messsage: "Select the team member's role?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "Manager:"
+                ],
+
+                when: ({ confirmRole }) => {
+                    if (confirmRole) {
+                        return true;
+                    } else {
+                        console.log("Please choice your team member's role!");
                     }
                 },
             },
 
             {
                 type: "input",
-                name: "IDNumber",
-                messsage: "What is the team Manager's Name?",
+                name: "id",
+                messsage: "What is the team members ID number?",
                 validate: (nameInput) => {
                     if (nameInput) {
                         return true;
                     } else {
-                        console.log("Please enter the team Manager's Name!");
+                        console.log("Please enter the team members ID number!");
                     }
                 },
             },
@@ -39,7 +60,7 @@ const promptUser = () => {
             {
                 type: "input",
                 name: "email",
-                messsage: "What is the team Manager's Name?",
+                messsage: "What is the team Members Email Addrss?",
                 validate: (nameInput) => {
                     if (nameInput) {
                         return true;
@@ -48,7 +69,64 @@ const promptUser = () => {
                     }
                 },
             },
-
         ])
-}
 
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office phone number";
+            }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "moreMembers"
+            }])
+
+
+        })
+
+        .then(function ({ roleInfo, moreMembers }) {
+            let newMember;
+            if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, roleInfo);
+            } else if (role === "Intern") {
+                newMember = new Intern(name, id, email, roleInfo);
+            } else {
+                newMember = new Manager(name, id, email, roleInfo);
+            }
+            employees.push(newMember);
+            addHtml(newMember)
+                .then(function () {
+                    if (moreMembers === "yes") {
+                        addMember();
+                    } else {
+                        finishHtml();
+                    }
+                });
+
+        });
+};
+
+function writeToFile(data) {
+    fs.writeFile("index.html", data, (err) => {
+        if (err) throw err;
+    })
+}
+// TODO: Create a function to initialize app
+function init() {
+    promptUser();
+}
+// Function call to initialize app
+init();
