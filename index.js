3e16f75233572e657aee69032b2cd43cf78e23e9
Denise <dmtuominen@gmail.com>
2021-08-27 21:04:29 -0600

    added switch for cards and push to html page

--- index.js
+++ index.js
@@ -70,51 +70,51 @@ const promptUser = () => {
         .then((answers) => {
             switch (answers.role) {
                 case "Employee":
-                    employees.push(new Employee(answers.name, answers.id, answers.email, answers.role)); 
-                        console.log(employees);
-                        newEmployee()   
+                    employees.push(new Employee(answers.name, answers.id, answers.email, answers.role));
+                    console.log(employees);
+                    newEmployee()
                     break;
                 case "Engineer":
-                    inquirer 
-                    .prompt([{
-                        type:"input",
-                        name: "github",
-                       message: "please enter your GitHub User Name",
-                    }])
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "github",
+                            message: "please enter your GitHub User Name",
+                        }])
 
-                    .then((extraAnswers) => {
-                        employees.push(new Engineer(answers.name, answers.id, answers.email, extraAnswers.github, answers.role)); 
-                        console.log(employees);
-                        newEmployee()   
-                    });   
+                        .then((extraAnswers) => {
+                            employees.push(new Engineer(answers.name, answers.id, answers.email, extraAnswers.github, answers.role));
+                            console.log(employees);
+                            newEmployee()
+                        });
                     break;
                 case "Intern":
-                    inquirer 
-                    .prompt([{
-                        type:"input",
-                        name: "school",
-                       message: "please enter your School",
-                    }])
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "school",
+                            message: "please enter your School",
+                        }])
 
-                    .then((extraAnswers) => {
-                        employees.push(new Intern(answers.name, answers.id, answers.email, extraAnswers.school, answers.role)); 
-                        console.log(employees);
-                        newEmployee()   
-                    });  
-                     break; 
+                        .then((extraAnswers) => {
+                            employees.push(new Intern(answers.name, answers.id, answers.email, extraAnswers.school, answers.role));
+                            console.log(employees);
+                            newEmployee()
+                        });
+                    break;
                 case "Manager":
-                    inquirer 
-                    .prompt([{
-                        type:"input",
-                        name: "officeNumber",
-                       message: "please enter your Office Number",
-                    }])
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "officeNumber",
+                            message: "please enter your Office Number",
+                        }])
 
-                    .then((extraAnswers) => {
-                        employees.push(new Manager(answers.name, answers.id, answers.email, extraAnswers.officeNumber, answers.role)); 
-                        console.log(employees);
-                        newEmployee()   
-                    });  
+                        .then((extraAnswers) => {
+                            employees.push(new Manager(answers.name, answers.id, answers.email, extraAnswers.officeNumber, answers.role));
+                            console.log(employees);
+                            newEmployee()
+                        });
                     break;
             }
 
@@ -142,10 +142,43 @@ function newEmployee() {
 
 // TODO: Create a function to write html file
 function writeToFile() {
-    fs.writeFile("index.html", data, (err) => {
+    var htmlData = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+       <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="./dist/style.css">
+
+        <title>Employee Contact Cards</title>
+    </head>
+    <body>`;
+
+    for (const card of employees) {
+        htmlData += "<div><h2>" + card.getName() + "<br>" + card.getRole() + "</h2>";
+        htmlData += "<p>ID: " + card.getId() + "</p>";
+        htmlData += "<p>Email: <a href='mailto:" + card.getEmail()+ "'>" + card.getEmail() + "</a></p>";
+        switch (card.getRole()) {
+            case "Engineer":
+                htmlData += "<p>GitHUb: " + card.getGithub() + "</p>";
+                break;
+            case "Intern":
+                htmlData += "<p>School: " + card.getSchool() + "</p>";
+                break;
+            case "Manager":
+                htmlData += "<p>Office Number: " + card.getOfficeNumber() + "</p>";
+                break;
+        }
+        htmlData += "</div>";
+    }
+
+    htmlData += `</body></html>`;
+
+    fs.writeFile("index.html", htmlData, (err) => {
         if (err) throw err;
 
-        console.log("Employee finished!");
+        console.log("Employee Cards Finished!");
     });
 }
 
