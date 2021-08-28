cd8c53fe905d708326708a4e48a3c21fee613737
Denise <dmtuominen@gmail.com>
2021-08-28 14:31:35 -0600

    trying to get style to work

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const Employee = require('./lib/Employee');
-const { resolve } = require("path");
-const { title } = require("process");
-
-
+const generatehtml = require("./lib/generatehtml");
 
 var employees = [];
 
@@ -59,7 +55,6 @@ const promptUser = () => {
                 name: "role",
                 message: "Select the team member's title?",
                 choices: [
-                    "Employee",
                     "Engineer",
                     "Intern",
                     "Manager"
@@ -69,11 +64,6 @@ const promptUser = () => {
 
         .then((answers) => {
             switch (answers.role) {
-                case "Employee":
-                    employees.push(new Employee(answers.name, answers.id, answers.email, answers.role));
-                    console.log(employees);
-                    newEmployee()
-                    break;
                 case "Engineer":
                     inquirer
                         .prompt([{
@@ -134,53 +124,10 @@ function newEmployee() {
                 promptUser()
 
             } else {
-                writeToFile();
+                writeToFile(generatehtml.writeToFile(employees));
             }
         })
 
-}
-
-// TODO: Create a function to write html file
-function writeToFile() {
-    var htmlData = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-       <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="./dist/style.css">
-
-        <title>Employee Contact Cards</title>
-
-    </head>
-    <body>`;
-
-    for (const card of employees) {
-        htmlData += "<div><h2>" + card.getName() + "<br>" + card.getRole() + "</h2>";
-        htmlData += "<p>ID: " + card.getId() + "</p>";
-        htmlData += "<p>Email: <a href='mailto:" + card.getEmail()+ "'>" + card.getEmail() + "</a></p>";
-        switch (card.getRole()) {
-            case "Engineer":
-                htmlData += "<p>GitHub: " + card.getGithub() + "</p>";
-                break;
-            case "Intern":
-                htmlData += "<p>School: " + card.getSchool() + "</p>";
-                break;
-            case "Manager":
-                htmlData += "<p>Office Number: " + card.getOfficeNumber() + "</p>";
-                break;
-        }
-        htmlData += "</div>";
-    }
-
-    htmlData += `</body></html>`;
-
-    fs.writeFile("index.html", htmlData, (err) => {
-        if (err) throw err;
-
-        console.log("Employee Cards Finished!");
-    });
 }
 
 // TODO: Create a function to initialize app
